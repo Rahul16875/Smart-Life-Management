2807c3ca90e4a221e9a780d245d24969699cf603
Rahul Agrawal <rahul.agrawal@leapfinance.com>
2026-02-01 23:27:29 +0530

    .

--- app/src/main/java/com/example/smartlifemanager/MainActivity.kt
+++ app/src/main/java/com/example/smartlifemanager/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SmartLifeManagerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+  
                 }
             }
         }
